51c8820c1e77e28d247e44886a20bda94d920e34
Bahram <bahram101@mail.ru>
2023-02-19 23:42:33 +0600

    login, header, localStorage

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,15 +4,14 @@ import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
-import styles from "./Login.module.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { fetchUserData, selectIsAuth } from "../../redux/slices/auth";
+import styles from "./Login.module.scss";
 
 export const Login = () => {
   const dispatch = useDispatch();
   const isAuth = useSelector(selectIsAuth);
-  console.log("isAuth", isAuth);
 
   const {
     register,
@@ -27,10 +26,19 @@ export const Login = () => {
     mode: "onChange",
   });
 
-  const onSubmit = (values) => {
-    dispatch(fetchUserData(values));
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchUserData(values));
+    console.log('data', data)
+    if (!data.payload) {
+      alert("Не удалось авторизоваться!");
+    } else {
+      window.localStorage.setItem("mern-token", data.payload.token);
+    }
   };
 
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
